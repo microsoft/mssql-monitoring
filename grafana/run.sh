5423403c5e87d77146aae0e3251d52d889b9d90e
Steven Schenider <stschn@microsoft.com>
2017-07-17 12:41:05 -0700

    Updating notes

--- grafana/run.sh
+++ grafana/run.sh
@@ -1,2 +1,3 @@
 #We use the grafana image that Grafana Labs provides http://docs.grafana.org/installation/docker/
+# If you wish to modify the port that Grafana runs on, you can do that here.
 sudo docker run -d -p 3000:3000 --name grafana grafana/grafana
--- influxdb/run.sh
+++ influxdb/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# By default, this will run without modification, but if you want to change where the data directory gets mapped, you can do that here
 # Make sure this folder exists on the host.
 # This directory from the host gets passed through to the docker container.
 INFLUXDB_HOST_DIRECTORY="/mnt/influxdb"
